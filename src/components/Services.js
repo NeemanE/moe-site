1043ff582f54eeb595828e9f6f5c107cfacc6a1d
Neeman Elchurafa <neemanchurafa@gmail.com>
2023-05-19 22:50:41 -0500

    fixed modal content and titles, starting on pricing styles

--- src/components/Services.js
+++ src/components/Services.js
@@ -6,6 +6,7 @@ import businessFormation from '../img/business-formation.jpg';
 import merchantServices from '../img/merchant-services.jpg';
 import mobileNotary from '../img/mobile-notary.jpg';
 import taxPreparation from '../img/tax-preparation.jpg';
+import '../App.css'
 
 const Services = () => {
     const [showModal1, setShowModal1] = useState(false);
@@ -45,7 +46,7 @@ const Services = () => {
                     <Card onClick={handleModal3Show}>
                         <Card.Img variant="top" src={businessFormation} style={{ height: '100%', objectFit: 'cover' }} />
                         <div style={{ backgroundColor: '#0f3d62', width: '100%', position: 'absolute', bottom: '0' }}>
-                            <Card.Title style={{ textAlign: 'center', color: 'white', padding: '0.5rem' }}>Business formation</Card.Title>
+                            <Card.Title style={{ textAlign: 'center', color: 'white', padding: '0.5rem' }}>Business Formation</Card.Title>
                         </div>                </Card>
                 </CardGroup>
             </div>
@@ -70,78 +71,85 @@ const Services = () => {
             </div>
             <Modal show={showModal1} onHide={handleModal1Close} centered>
                 <Modal.Header closeButton>
-                    <Modal.Title style={{ textAlign: 'center' }}>Your business credit can be as important as your personal credit. Creditors and suppliers are increasingly using business credit reports to make lending and credit decisions. Establishing business credit may seem like a daunting process—especially if your business is new. BUT , at Protocall Financial, We Will be with you every step of the way to help you achieve your business Credit Goals.
-                    </Modal.Title>
+                    <Modal.Title>Business Credit</Modal.Title>
                 </Modal.Header>
-                <Modal.Body style={{ textAlign: 'center' }}>
-                    <p>Card 1 Modal Body</p>
+                <Modal.Body>
+                    <p>Your business credit can be as important as your personal credit. Creditors and suppliers are increasingly using business credit reports to make lending and credit decisions. Establishing business credit may seem like a daunting process—especially if your business is new. BUT, at Protocall Financial, We Will be with you every step of the way to help you achieve your business Credit Goals.</p>
                 </Modal.Body>
             </Modal>
             <Modal show={showModal2} onHide={handleModal2Close} centered>
                 <Modal.Header closeButton>
-                    <Modal.Title style={{ textAlign: 'center' }}>You can legally repair your credit utilizing consumer protections laws such as the Fair Credit Reporting Act, Fair Debt Collection Practices Act, and Much more. Protocall Financial not only works to repair your credit, we provide you the tools you need to educate you for a lifetime of success. Our proven credit repair process will do almost all the work for you while giving you support from start to finish.
-                    </Modal.Title>
+                    <Modal.Title>Personal Credit Restoration</Modal.Title>
                 </Modal.Header>
-                <Modal.Body style={{ textAlign: 'center' }}>
-                    <p>Card 2 Modal Body</p>
+                <Modal.Body>
+                    <p>You can legally repair your credit utilizing consumer protections laws such as the Fair Credit Reporting Act, Fair Debt Collection Practices Act, and Much more. Protocall Financial not only works to repair your credit, we provide you the tools you need to educate you for a lifetime of success. Our proven credit repair process will do almost all the work for you while giving you support from start to finish.</p>
                 </Modal.Body>
             </Modal>
             <Modal show={showModal3} onHide={handleModal3Close} centered>
                 <Modal.Header closeButton>
-                    <Modal.Title style={{ textAlign: 'center' }}>Business formation is a necessary early step when starting a business, whether you're registering a simple DBA, incorporating or forming a partnership. The way in which your business is formed will determine the personal liability of the founders, how taxes are paid, and other important details.
-
-
-
-                        There are four main types of business structures in the U.S: sole proprietorship, partnership, limited liability and corporation. Each structure has different tax, income and liability implications for businesses owners and their companies.
-
-
-
-                        TOP REASONS FOR Business FORMATION :
-
-
-
-                        1. Limited Liability For Shareholders
-                        2. Raising Capital
-                        3. Flexibility of Ownership
-                        4. Corporate Deductions
-                        5. Asset Protection
-                        6. Lower IRS Audit Probability
-                        7. Credibility
-                        8. Central Management and Corporate Structure
-                        9. Fiscal Year / Income Splitting
-                        10. Perpetual Duration
+                    <Modal.Title>Business Formation
                     </Modal.Title>
                 </Modal.Header>
-                <Modal.Body style={{ textAlign: 'center' }}>
-                    <p>Card 3 Modal Body</p>
+                <Modal.Body>
+                    <p>
+                        Business formation is a necessary early step when starting a business, whether you're registering a simple DBA, incorporating or forming a partnership. The way in which your business is formed will determine the personal liability of the founders, how taxes are paid, and other important details.
+                    </p>
+
+                    <p>
+                        There are four main types of business structures in the U.S: sole proprietorship, partnership, limited liability and corporation. Each structure has different tax, income and liability implications for businesses owners and their companies.
+                    </p>
+                    <p>
+                        <span style={{ fontWeight: 'bold' }}>Top Reasons for Business Formation :</span>
+
+                        <br />
+                        1. Limited Liability For Shareholders
+                        <br />
+                        2. Raising Capital
+                        <br />
+                        3. Flexibility of Ownership
+                        <br />
+                        4. Corporate Deductions
+                        <br />
+                        5. Asset Protection
+                        <br />
+                        6. Lower IRS Audit Probability
+                        <br />
+                        7. Credibility
+                        <br />
+                        8. Central Management and Corporate Structure
+                        <br />
+                        9. Fiscal Year / Income Splitting
+                        <br />
+                        10. Perpetual Duration
+                    </p>
                 </Modal.Body>
             </Modal>
             <Modal show={showModal4} onHide={handleModal4Close} centered>
                 <Modal.Header closeButton>
-                    <Modal.Title style={{ textAlign: 'center' }}>There are many services out there for you to choose from. Every company has their own pricing and options, but when it comes time to make a decision, you want to get the best option for your business. We want you to choose us TODAY, so give us a call today and allow Protocall Financial to put more money in your pocket by offering you the best Merchant Service rates in the market!
+                    <Modal.Title>Merchant Services
                     </Modal.Title>
                 </Modal.Header>
-                <Modal.Body style={{ textAlign: 'center' }}>
-                    <p>Card 4 Modal Body</p>
+                <Modal.Body>
+                    <p>There are many services out there for you to choose from. Every company has their own pricing and options, but when it comes time to make a decision, you want to get the best option for your business. We want you to choose us TODAY, so give us a call today and allow Protocall Financial to put more money in your pocket by offering you the best Merchant Service rates in the market!</p>
                 </Modal.Body>
             </Modal>
             <Modal show={showModal5} onHide={handleModal5Close} centered>
                 <Modal.Header closeButton>
-                    <Modal.Title style={{ textAlign: 'center' }}>Card 5 Modal</Modal.Title>
+                    <Modal.Title>Mobile Notary</Modal.Title>
                 </Modal.Header>
-                <Modal.Body style={{ textAlign: 'center' }}>
-                    <p>Card 5 Modal Body</p>
+                <Modal.Body>
+                    <p>Need a document notarized but don't want the hassle of finding one? Just give us a call! We offer notary services directly to you, any part of town, any time of day.</p>
                 </Modal.Body>
             </Modal>
             <Modal show={showModal6} onHide={handleModal6Close} centered>
                 <Modal.Header closeButton>
-                    <Modal.Title style={{ textAlign: 'center' }}>Card 6 Modal</Modal.Title>
+                    <Modal.Title>Tax Preparation Services</Modal.Title>
                 </Modal.Header>
-                <Modal.Body style={{ textAlign: 'center' }}>
-                    <p>Card 6 Modal Body</p>
+                <Modal.Body>
+                    <p>Specializing in Individual and Small Business/LLC returns, we make tax season easy for you, and your wallet! Just bring us your documents and let us handle the rest. </p>
                 </Modal.Body>
             </Modal>
-        </div>
+        </div >
     );
 };
 
