e0e28445c456e21da5362d7ca54a3098b7f19759
Neeman Elchurafa <neemanchurafa@gmail.com>
2023-06-17 21:24:55 -0500

    added react-router-dom and implemented second route. have a lot to catch up on from data wipe. always push ur commits..

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
-import React from 'react';
-import './App.css';
-import Contact from './components/Contact';
-import Faq from './components/Faq';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
-import Services from './components/Services';
-import Pricing from './components/Pricing';
-import Affiliates from './components/Affiliates';
-import Reviews from './components/Reviews';
-import HeroCard from './components/HeroCard';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import privacyPolicyData from './privacyPolicy.json';
-import termsOfUseData from './termsOfUse.json';
-import returnPolicyData from './returnPolicy.json';
+import React from "react";
+import "./App.css";
+import Contact from "./components/Contact";
+import Faq from "./components/Faq";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Services from "./components/Services";
+import Pricing from "./components/Pricing";
+import Affiliates from "./components/Affiliates";
+import Credit from "./components/Credit";
+import Reviews from "./components/Reviews";
+import HeroCard from "./components/HeroCard";
+import "bootstrap/dist/css/bootstrap.min.css";
+import privacyPolicyData from "./privacyPolicy.json";
+import termsOfUseData from "./termsOfUse.json";
+import returnPolicyData from "./returnPolicy.json";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 export default function App() {
   return (
-    <main>
+    <Router>
       <Header />
       <Navbar />
-      <Hero />
-      <HeroCard />
-      <Services />
-      <Pricing />
-      <Faq />
-      <Reviews />
-      <Contact />
-      <Footer
-        privacyPolicyData={privacyPolicyData}
-        termsOfUseData={termsOfUseData}
-        returnPolicyData={returnPolicyData} />
-    </main>
+      <Routes>
+        {/* Define routes */}
+        <Route path="/credit" element={<Credit />} />
+        <Route
+          path="/"
+          element={
+            <main>
+              <Hero />
+              <HeroCard />
+              <Services />
+              <Pricing />
+              <Faq />
+              <Reviews />
+              <Contact />
+              <Footer
+                privacyPolicyData={privacyPolicyData}
+                termsOfUseData={termsOfUseData}
+              />
+            </main>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
---
+++ src/components/Credit.js
@@ -0,0 +1,13 @@
+// components/Credit.js
+import React from 'react';
+
+const Credit = () => {
+    return (
+        <div>
+            {/* Content of your Credit page goes here */}
+            <h1>Credit Page</h1>
+        </div>
+    );
+};
+
+export default Credit;
